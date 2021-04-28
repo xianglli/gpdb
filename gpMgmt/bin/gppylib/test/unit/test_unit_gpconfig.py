0386e33e9af5a4df4fcb5240f9f4dc232ce3ae27
Divyesh Vanjare <divyeshddv@gmail.com>
2021-04-27 17:03:21 -0700

    Revert "gpconfig: aborting a config update with down host should exit properly (#11765)"
    This reverts commit 9f3dfc77dec76040e7c9f6826e5cf76eb8deca84.

--- gpMgmt/bin/gppylib/test/unit/test_unit_gpconfig.py
+++ gpMgmt/bin/gppylib/test/unit/test_unit_gpconfig.py
@@ -8,6 +8,7 @@ import sys
 import tempfile
 
 from gppylib.gparray import Segment, GpArray, SegmentPair
+from gppylib.gphostcache import GpHost
 from gpconfig_modules.parse_guc_metadata import ParseGuc
 
 from gp_unittest import *
@@ -51,12 +52,18 @@ class GpConfig(GpTestCase):
         self.os_env["MASTER_DATA_DIRECTORY"] = self.temp_dir
         self.os_env["GPHOME"] = self.temp_dir
         self.gparray = self._create_gparray_with_2_primary_2_mirrors()
+        self.host_cache = Mock()
 
+        self.host = GpHost('localhost')
         seg = SegmentPair()
         db = self.gparray.master
         seg.addPrimary(db)
         seg.datadir = self.gparray.master.datadir
         seg.hostname = 'localhost'
+        self.host.addDB(seg)
+
+        self.host_cache.get_hosts.return_value = [self.host]
+        self.host_cache.ping_hosts.return_value = []
 
         self.master_file = Mock(name='master')
         self.master_file.get_value.return_value = 'foo'
@@ -76,9 +83,9 @@ class GpConfig(GpTestCase):
             patch('gpconfig.dbconn.connect', return_value=self.conn),
             patch('gpconfig.dbconn.execSQL', return_value=self.cursor),
             patch('gpconfig.dbconn.execSQLForSingleton', side_effect=singleton_side_effect),
+            patch('gpconfig.GpHostCache', return_value=self.host_cache),
             patch('gpconfig.GpArray.initFromCatalog', return_value=self.gparray),
-            patch('gpconfig.WorkerPool', return_value=self.pool),
-            patch('gpconfig.get_unreachable_segment_hosts', return_value=[])
+            patch('gpconfig.WorkerPool', return_value=self.pool)
         ])
         sys.argv = ["gpconfig"]  # reset to relatively empty args list
 
@@ -172,7 +179,7 @@ class GpConfig(GpTestCase):
 
         self.subject.do_main()
 
-        self.assertEqual(self.pool.addCommand.call_count, 5)
+        self.pool.addCommand.assert_called_once()
         self.pool.join.assert_called_once_with()
         self.pool.check_results.assert_called_once_with()
         self.pool.haltWork.assert_called_once_with()
@@ -228,9 +235,11 @@ class GpConfig(GpTestCase):
         # mocked values in the files
         self.pool.getCompletedItems.return_value.append(seg_1)
 
+        self.host_cache.get_hosts.return_value.extend([self.host, self.host])
+
         self.subject.do_main()
 
-        self.assertEqual(self.pool.addCommand.call_count, 5)
+        self.assertEqual(self.pool.addCommand.call_count, 3)
         self.assertEqual(self.subject.LOGGER.error.call_count, 0)
         self.assertIn("WARNING: GUCS ARE OUT OF SYNC", mock_stdout.getvalue())
         self.assertIn("bar", mock_stdout.getvalue())
@@ -251,9 +260,11 @@ class GpConfig(GpTestCase):
         # mocked values in the files
         self.pool.getCompletedItems.return_value.append(seg_1)
 
+        self.host_cache.get_hosts.return_value.extend([self.host, self.host])
+
         self.subject.do_main()
 
-        self.assertEqual(self.pool.addCommand.call_count, 5)
+        self.assertEqual(self.pool.addCommand.call_count, 3)
         self.assertEqual(self.subject.LOGGER.error.call_count, 0)
         self.assertIn("WARNING: GUCS ARE OUT OF SYNC", mock_stdout.getvalue())
         self.assertIn("bar", mock_stdout.getvalue())
@@ -272,12 +283,12 @@ class GpConfig(GpTestCase):
         self.subject.do_main()
 
         self.subject.LOGGER.info.assert_called_with("completed successfully with parameters '-c my_property_name -v 100 -m 20'")
-        self.assertEqual(self.pool.addCommand.call_count, 5)
+        self.assertEqual(self.pool.addCommand.call_count, 2)
         segment_command = self.pool.addCommand.call_args_list[0][0][0]
         self.assertTrue("my_property_name" in segment_command.cmdStr)
         value = base64.urlsafe_b64encode(pickle.dumps("100"))
         self.assertTrue(value in segment_command.cmdStr)
-        master_command = self.pool.addCommand.call_args_list[4][0][0]
+        master_command = self.pool.addCommand.call_args_list[1][0][0]
         self.assertTrue("my_property_name" in master_command.cmdStr)
         value = base64.urlsafe_b64encode(pickle.dumps("20"))
         self.assertTrue(value in master_command.cmdStr)
@@ -315,10 +326,10 @@ class GpConfig(GpTestCase):
         self.subject.do_main()
 
         self.subject.LOGGER.info.assert_called_with("completed successfully with parameters '-c my_hidden_guc_name -v 100 --skipvalidation'")
-        self.assertEqual(self.pool.addCommand.call_count, 5)
+        self.assertEqual(self.pool.addCommand.call_count, 2)
         segment_command = self.pool.addCommand.call_args_list[0][0][0]
         self.assertTrue("my_hidden_guc_name" in segment_command.cmdStr)
-        master_command = self.pool.addCommand.call_args_list[4][0][0]
+        master_command = self.pool.addCommand.call_args_list[1][0][0]
         self.assertTrue("my_hidden_guc_name" in master_command.cmdStr)
         value = base64.urlsafe_b64encode(pickle.dumps("100"))
         self.assertTrue(value in master_command.cmdStr)
